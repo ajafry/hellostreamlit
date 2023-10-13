75b900c864515b9cf1f1acd1f11bc49abd463ede
ajafry <ajafry@gmail.com>
2023-10-13 14:01:47 -0700

    stuff

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import msal
 
 client_id = os.environ.get('CLIENT_ID', '***NO CLIENT ID***')
 client_secret = os.environ.get('CLIENT_SECRET', '***NO CLIENT SECRET***')
+authority = os.environ.get('AUTHORITY', '***NO AUTHORITY***')
+
 scopes=["https://graph.microsoft.com/.default"]
 
 app = msal.ConfidentialClientApplication(
-    "77e2f8e5-5b78-47c5-abc1-fbb6006e1633", authority="https://login.microsoftonline.com/fd58167c-8662-4cd9-8797-550a074ef51d",
-    client_credential="480a3cdd-7eea-42ed-b991-29feb2863ab2",
+    client_id, authority=authority,
+    client_credential=client_secret,
     # token_cache=...  # Default cache is in memory only.
                        # You can learn how to use SerializableTokenCache from
                        # https://msal-python.readthedocs.io/en/latest/#msal.SerializableTokenCache
