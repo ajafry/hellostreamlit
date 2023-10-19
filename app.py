bd1ee08bd56d8c73e1705af3bdda8f6a5823466e
ajafry <ajafry@gmail.com>
2023-10-19 10:30:15 -0700

    fixed module import

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from streamlit.script_run_context import get_script_run_ctx
+#from streamlit.script_run_context import get_script_run_ctx
+#>>>from streamlit.scriptrunner.script_run_context import get_script_run_ctx
+#from streamlit.runtime.scriptrunner import get_script_run_ctx
 from streamlit.server.server import Server
 import os
 import msal
